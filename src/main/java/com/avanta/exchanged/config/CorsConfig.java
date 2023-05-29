6c1ec7626d1cae46dfcb13d2c4b16e31087128c0
Jhonatan <jhonatan98181@gmail.com>
2023-05-29 04:35:10 -0500

    gitignore archivos log

--- src/main/java/com/avanta/exchanged/config/CorsConfig.java
+++ src/main/java/com/avanta/exchanged/config/CorsConfig.java
@@ -2,9 +2,11 @@ package com.avanta.exchanged.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
+@EnableWebFlux
 public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
